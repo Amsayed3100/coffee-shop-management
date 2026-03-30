ec1f9ccbd60386e09f69ea29504838da002afc54
NazmulHasan660 <nazmulhasanmahady992@gmail.com>
2026-03-30 19:16:23 +0600

    Fix auto admin creation import issue

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -15,15 +12,13 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-    
-    from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
 def create_default_admin():
     if not User.objects.filter(username="admin").exists():
         User.objects.create_superuser(
             username="admin",
             email="admin@gmail.com",
-            password="admin123"
+            password="admin123",
+            role="ADMIN",
         )
