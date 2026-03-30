a9856c1e50a69baf49ec8c8bc22ef02790012bd6
NazmulHasan660 <nazmulhasanmahady992@gmail.com>
2026-03-30 19:11:43 +0600

    Auto create admin for production

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -2,4 +2,12 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from .models import create_default_admin
+        try:
+            create_default_admin()
+        except Exception:
+            pass
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -15,3 +15,15 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+    from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def create_default_admin():
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="admin123"
+        )
